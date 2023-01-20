ad4cef3bbafdcb39408ee2b6695069a589e9cfec
flashboy366@ya.ru <platonovk1998@ya.ru>
2023-01-20 15:30:44 +0500

    production fix

--- server/config/email.cjs
+++ server/config/email.cjs
@@ -1,15 +1,15 @@
-// // DEV
-// module.exports = {
-//   SERVICE: 'yandex',
-//   USER: 'platonovk1998@ya.ru',
-//   PASS: '1UzP1z',
-//   OPERATOR_ADDRESS: 'flashboy366@ya.ru'
-// }
-
-// PROD
+// DEV
 module.exports = {
-  SERVICE: 'Mail.ru',
-  USER: 'dostavim.kz@mail.ru',
-  PASS: 'Dostavim52144',
-  OPERATOR_ADDRESS: 'dostavim.kz@mail.ru'
+  SERVICE: 'yandex',
+  USER: 'platonovk1998@ya.ru',
+  PASS: '1UzP1z',
+  OPERATOR_ADDRESS: 'platonovk1998@ya.ru'
 }
+
+// // PROD
+// module.exports = {
+//   SERVICE: 'Mail.ru',
+//   USER: 'dostavim.kz@mail.ru',
+//   PASS: 'Dostavim52144',
+//   OPERATOR_ADDRESS: 'dostavim.kz@mail.ru'
+// }
