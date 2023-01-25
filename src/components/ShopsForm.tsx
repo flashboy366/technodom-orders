94f47125852ac2010e0bbc8a72bdce63802415a6
flashboy366@ya.ru <platonovk1998@ya.ru>
2023-01-26 01:58:08 +0500

    interface update
    - added markup for shop select functionality

--- src/components/ShopsForm.tsx
+++ src/components/ShopsForm.tsx
@@ -1,30 +1,90 @@
-import { Stack, Typography } from '@mui/material'
-import TechnodomLink from './brandLinks/TechnodomLink'
-import MieleLink from './brandLinks/MieleLink'
+import {
+  Box,
+  Button,
+  FormControl,
+  FormControlLabel,
+  FormLabel,
+  Radio,
+  RadioGroup,
+  Stack,
+} from '@mui/material'
 import FormWrapper from './FormWrapper'
 import useIsMediaWidth from '../hooks/useIsMediaWidth'
 import { desktopWidthSelector } from '../util/materialui'
+import Image from 'mui-image'
+import { useFormContext } from 'react-hook-form'
+import { useAppDispatch } from '../hooks/redux'
+import { resetProductsState } from '../redux/reducers/orderProductsReducer'
 
 const ShopsForm = () => {
   const [iseDesktopMedia] = useIsMediaWidth(desktopWidthSelector())
 
+  const { reset: resetForm } = useFormContext()
+  const dispatch = useAppDispatch()
+
+  const switchShopHandler = () => {
+    resetForm()
+    dispatch(resetProductsState())
+  }
+
   return (
     <FormWrapper title="">
-      <Stack
-        direction={iseDesktopMedia ? 'row' : 'column'}
-        spacing={iseDesktopMedia ? 1 : 5}
-        alignItems="center"
-        justifyContent="space-around"
-      >
+      <Stack spacing={3} alignItems="center" justifyContent="space-around">
         <Stack
           direction={iseDesktopMedia ? 'row' : 'column'}
           alignItems="center"
           spacing={2}
         >
-          <Typography>Выберите поставщика:</Typography>
-          <TechnodomLink />
-          <MieleLink />
+          <FormControl sx={{ gap: 2 }}>
+            <FormLabel sx={{ alignSelf: 'center' }}>
+              Выберите поставщика
+            </FormLabel>
+            <RadioGroup row onChange={switchShopHandler} sx={{ gap: 3 }}>
+              <FormControlLabel
+                control={<Radio />}
+                label={
+                  <Image
+                    src="technodom_logo.png"
+                    width={245}
+                    height={60}
+                    fit="none"
+                    bgColor="white"
+                  />
+                }
+                value="technodom"
+              />
+              <FormControlLabel
+                control={<Radio />}
+                label={
+                  <Image
+                    src={'miele_logo.png'}
+                    height={60}
+                    width={200}
+                    fit="none"
+                  />
+                }
+                value="miele"
+              />
+              <FormControlLabel
+                control={<Radio />}
+                label={
+                  <Box
+                    width={250}
+                    justifyContent="center"
+                    position="relative"
+                    left={-30}
+                  >
+                    <Image src={'mechta_logo.png'} height={60} fit="cover" />
+                  </Box>
+                }
+                value="mechta"
+              />
+            </RadioGroup>
+          </FormControl>
         </Stack>
+        <Button variant="outlined" sx={{}} disabled>
+          Перейти на сайт поставщика
+        </Button>
       </Stack>
     </FormWrapper>
   )
