26503ac1455add2a405c3300919a9251ca8057eb
flashboy366@ya.ru <platonovk1998@ya.ru>
2023-03-18 17:28:21 +0500

    updated whole app for iHerb functionality WIP

--- src/constants/server.ts
+++ src/constants/server.ts
@@ -1,11 +1,11 @@
-// DEV
-const SERVER = {
-  URL: 'http://localhost:8000/',
-}
-//
-// // PROD
+// // DEV
 // const SERVER = {
-//   URL: 'https://dostavimkz.ru/',
+//   URL: 'http://localhost:8000/',
 // }
+
+// PROD
+const SERVER = {
+  URL: 'https://dostavimkz.ru/',
+}
 
 export default SERVER
